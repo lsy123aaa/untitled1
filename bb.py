346f04066acf036b754ce0a0a420dbe864820955
lsy123aaa <2234272317@qq.com>
2020-03-26 23:49:53 +0800

    修改1

---
+++ bb.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@author: LSY123
+@file: bb.py
+@time: 2020/03/26
+@desc: 
+"""
---
+++ main.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@author: LSY123
+@file: main.py
+@time: 2020/03/26
+@desc: 
+"""
+
+print("aaaaaa")
